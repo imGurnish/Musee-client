4e639f9015781d59651253554cd090f353feaa58
imGurnish <2023kucp1082@iiikota.ac.in>
2026-05-23 07:48:58 +0530

    Android Sign Fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,88 +1,58 @@
-import java.util.Properties
 import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 val keystoreProperties = Properties()
-
-val keystorePropertiesFile =
-    rootProject.file("key.properties")
-
+val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
-
-    keystoreProperties.load(
-        FileInputStream(keystorePropertiesFile)
-    )
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
-
     namespace = "com.example.musee"
-
     compileSdk = flutter.compileSdkVersion
-
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility =
-            JavaVersion.VERSION_11
-
-        targetCompatibility =
-            JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget =
-            JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-
-        applicationId = "com.example.musee"
-
-        minSdk = flutter.minSdkVersion
-
-        targetSdk = flutter.targetSdkVersion
-
-        versionCode = flutter.versionCode
-
-        versionName = flutter.versionName
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     signingConfigs {
-
         create("release") {
-
-            keyAlias =
-                keystoreProperties["keyAlias"] as String
-
-            keyPassword =
-                keystoreProperties["keyPassword"] as String
-
-            storeFile =
-                file(
-                    keystoreProperties["storeFile"] as String
-                )
-
-            storePassword =
-                keystoreProperties["storePassword"] as String
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.musee"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
     buildTypes {
-
-        getByName("release") {
-
-            signingConfig =
-                signingConfigs.getByName("release")
-
-            isMinifyEnabled = false
-
-            isShrinkResources = false
+        release {
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
